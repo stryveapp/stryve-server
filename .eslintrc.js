9326ff564756db25f3922ff0d4285a4325220197
Nicholas Law <nick@nicholaslaw.com.au>
2017-04-18 08:42:56 +1000

    Use Stryve eslint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,3 @@
 module.exports = {
-  root: true,
-  parserOptions: {
-    sourceType: 'module',
-  },
-  env: {
-    es6: true,
-    node: true,
-  },
-  extends: 'airbnb-base',
-  'rules': {
-    'semi': ["error", "never"],
-    'no-underscore-dangle': ["error", { "allowAfterThis": true }],
-    'global-require': 0,
-    'class-methods-use-this': 0,
-    'import/no-unresolved': 0,
-    'no-param-reassign': 0,
-    'no-shadow': 0,
-  }
+  extends: 'stryve',
 }
